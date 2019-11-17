16599bafb09733d23fe2286bfbf7cbfb778f756b
semtax <tlqaksqhr@naver.com>
2019-11-17 21:14:19 +0900

    implement favorite button

--- app/src/main/java/com/example/flxrexample/quest/QuestMainViewModel.kt
+++ app/src/main/java/com/example/flxrexample/quest/QuestMainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.flxrexample.quest
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.flxrexample.quest_model.Quest
 import com.example.flxrexample.quest_model.QuestRepository
 
 class QuestMainViewModel : ViewModel() {
@@ -12,4 +13,6 @@ class QuestMainViewModel : ViewModel() {
     fun getQuests() = allQuests
 
     fun getQuest(id: Int) = repository.getQuest(id)
+
+    fun updateQuest(quest: Quest) = repository.updateQuest(quest)
 }
